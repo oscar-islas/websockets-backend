46c1ff253e991ba9d927ecb11171b19a40719d22
Oscar Islas <oislasreyes@gmail.com>
2020-06-17 21:21:19 -0500

    Creando el endpoint para recibir un tweet del cliente

--- index.js
+++ index.js
@@ -1,39 +1,50 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
 const app = express();
 
-const server = app.listen(8080, () => {    
-    console.log("Servidor iniciado en el puerto 8080");    
-});
+app.use(cors());
 
-let clients = 0;
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 8080;
+
+const server = app.listen(PORT);
 
 const io = require('socket.io')(server);
-io.on('connection', (socket) => {
-    console.log("Usuario conectado");
-    clients++;
-    console.log(clients);
-    io.emit('feed', {tweets: tweets});
-    socket.on("disconnect", () => {
-        clients--;
-        console.log("El cliente se ha desconectado, no clientes:", clients);
-    })
 
-});
-
+let connectedClients = 0;
 
 let tweets = [];
 
-app.use(bodyParser.json());
-app.use(cors());
-app.post('/tweets', (req, res) => {
-    const tweet = req.body.tweet;
-    tweets.push(tweet);
-    console.log(tweets);
+//Evento para manejar las conexiones del cliente
+io.on('connection', (socket) => {
+    connectedClients++; //Contador de clientes conectados
+    let address = socket.handshake.headers;
+    console.log("Se ha establecido una conexión con el cliente", socket.handshake.headers);
+    console.log("Clientes conectados", connectedClients);
+    io.emit('new_connection', {clients: connectedClients});
     io.emit('feed', {tweets: tweets});
-    res.json({
-        message: "Tweet agregado satisfactoriamente"
+    socket.on('disconnect', () => { //Manejador de evento en caso de que un cliente se haya desconectado
+        connectedClients--;
+        console.log("Se ha desconectado un cliente");
+        console.log("Clientes conectados", connectedClients);
+        io.emit('new_connection', {clients: connectedClients});
     });
+});
+
+app.get('/', (req, res) => {
+    res.json({message: 'Ejemplo usando websockets'});
+});
+
+app.delete('/tweet', (req, res) => {
+    //obtenemos el indice del tweet que se desea borrar
+});
+
+app.post('/tweet', (req, res) => {
+    tweets.push(req.body.tweet);
+    io.emit('feed', {tweets: tweets});
+    res.json({message: 'Se ha publicado tu Tweet'});
 });
 
